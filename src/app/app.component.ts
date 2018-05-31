c9edb48e7a4d238fc42cbeeec9f35dbc00eafc04
Zsolt Kerekes <zsolt_kerekes@epam.com>
2018-05-31 10:03:46 +0200

    added 'cookie consent', made some minor changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { animate, animation, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
+import {animate, animation, style, transition, trigger} from '@angular/animations';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NavigationEnd, Router} from '@angular/router';
 
 const slideAnimation = animation([
-  style({ opacity: .3, position: 'absolute', left: 0, right: 0, transform: 'translate3d(40%,0,0)' }),
-  animate('600ms ease-in-out', style({ opacity: 1, transform: 'translate3d(17.5%,0,0)' }))
+  style({opacity: .3, position: 'absolute', left: 0, right: 0, transform: 'translate3d(40%,0,0)'}),
+  animate('600ms ease-in-out', style({opacity: 1, transform: 'translate3d(17.5%,0,0)'}))
 ]);
+
 @Component({
   selector: 'cv-root',
   templateUrl: './app.component.html',
@@ -16,7 +17,12 @@ const slideAnimation = animation([
     ])
   ]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  cookieLawSeen: boolean;
+
+  @ViewChild('cookieLaw')
+  cookieLawEl: any;
 
   constructor(public router: Router) {
     this.router.events.subscribe(event => {
@@ -30,7 +36,11 @@ export class AppComponent {
   prepareRouteTransition(outlet) {
     const animation = outlet.activatedRouteData['animation'] || {};
     return animation['value'] || null;
-}
+  }
+
+  ngOnInit() {
+    this.cookieLawSeen = this.cookieLawEl.cookieLawSeen;
+  }
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,32 @@
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AboutMeTextComponent } from './blocks/about-me-text/about-me-text.component';
-import { ContactFormComponent } from './blocks/contact-form/contact-form.component';
-import { CountOnMeComponent } from './blocks/count-on-me/count-on-me.component';
-import { ExperienceProgressComponent } from './blocks/experience-progress/experience-progress.component';
-import { FooterComponent } from './blocks/footer/footer.component';
-import { ProfessionalExperienceListComponent } from './blocks/professional-experience-list/professional-experience-list.component';
-import { ReferenceListComponent } from './blocks/reference-list/reference-list.component';
-import { StudyListComponent } from './blocks/study-list/study-list.component';
-import { VideoComponent } from './blocks/video/video.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { Page404Component } from './pages/page-404/page-404.component';
-import { ProfessionalExperienceComponent } from './pages/professional-experience/professional-experience.component';
-import { ReferenceComponent } from './pages/reference/reference.component';
-import { StudyComponent } from './pages/study/study.component';
-import { MaterialModule } from './shared/modules/material/material.module';
-import { FilterPipe } from './shared/pipes/filter.pipe';
-import { DataStorageService } from './shared/services/data-storage.service';
-import { SmoothScrollService } from './shared/services/smooth-scroll.service';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AboutMeTextComponent} from './blocks/about-me-text/about-me-text.component';
+import {ContactFormComponent} from './blocks/contact-form/contact-form.component';
+import {CountOnMeComponent} from './blocks/count-on-me/count-on-me.component';
+import {ExperienceProgressComponent} from './blocks/experience-progress/experience-progress.component';
+import {FooterComponent} from './blocks/footer/footer.component';
+import {ProfessionalExperienceListComponent} from './blocks/professional-experience-list/professional-experience-list.component';
+import {ReferenceListComponent} from './blocks/reference-list/reference-list.component';
+import {StudyListComponent} from './blocks/study-list/study-list.component';
+import {VideoComponent} from './blocks/video/video.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {AboutMeComponent} from './pages/about-me/about-me.component';
+import {ContactsComponent} from './pages/contacts/contacts.component';
+import {Page404Component} from './pages/page-404/page-404.component';
+import {ProfessionalExperienceComponent} from './pages/professional-experience/professional-experience.component';
+import {ReferenceComponent} from './pages/reference/reference.component';
+import {StudyComponent} from './pages/study/study.component';
+import {MaterialModule} from './shared/modules/material/material.module';
+import {FilterPipe} from './shared/pipes/filter.pipe';
+import {DataStorageService} from './shared/services/data-storage.service';
+import {SmoothScrollService} from './shared/services/smooth-scroll.service';
+import {CookieLawModule} from 'angular2-cookie-law';
 
 @NgModule({
   declarations: [
@@ -54,7 +55,8 @@ import { SmoothScrollService } from './shared/services/smooth-scroll.service';
     HttpModule,
     AppRoutingModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    CookieLawModule
   ],
   providers: [
     DataStorageService,
@@ -64,4 +66,5 @@ import { SmoothScrollService } from './shared/services/smooth-scroll.service';
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+}
