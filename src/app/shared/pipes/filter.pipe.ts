680f2b5be70186fd72849414eb5f2d899c777abc
Kerekes Zsolt <kerekes.zsolt.job@gmail.com>
2021-04-24 12:15:26 +0200

    some more fine tuning

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -1,16 +1,23 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'filter'
+  name: "filter",
 })
 export class FilterPipe implements PipeTransform {
-
-  transform(value, search = '') {
+  transform(value, search = "") {
     if (search.length === 0) {
       return value;
     } else {
       return value.filter(
-        row => row.name.toUpperCase().indexOf(search.toUpperCase()) !== -1 ? true : false
+        (row) =>
+          (row.name &&
+          row.name.toUpperCase().indexOf(search.toUpperCase()) !== -1
+            ? true
+            : false) ||
+          (row.description &&
+          row.description.toUpperCase().indexOf(search.toUpperCase()) !== -1
+            ? true
+            : false)
       );
     }
   }
